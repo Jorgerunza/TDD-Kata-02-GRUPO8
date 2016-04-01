28ee97675681c8bfeb4c64329ed7089786773968
Jorge David Runza Gualdron <jd.runza@uniandes.edu.co>
2016-03-31 23:21:31 -0500

    Paso 2- Test para probar registro independiente (Rojo)

--- buscoayuda/componentes/Functional_test.py
+++ buscoayuda/componentes/Functional_test.py
@@ -14,3 +14,40 @@ class FunctionalTest(TestCase):
         self.browser.get('http://localhost:8000')
         self.assertIn('Busco Ayuda', self.browser.title)
 
+
+    def test_registro(self):
+        self.browser.get('http://localhost:8000')
+        link = self.browser.find_element_by_id('id_register')
+        link.click()
+
+        self.browser.implicitly_wait(3)
+
+        nombre = self.browser.find_element_by_id('nombre')
+        nombre.send_keys('Jorge')
+
+        apellidos = self.browser.find_element_by_id('apellidos')
+        apellidos.send_keys('Runza')
+
+        self.browser.find_element_by_xpath("//select[@id='idServicio']/option[text()='Plomeria']").click()
+
+        aniosExperiencia = self.browser.find_element_by_id('aniosExperiencia')
+        aniosExperiencia.send_keys('1')
+
+        telefono = self.browser.find_element_by_id('telefono')
+        telefono.send_keys('6718771')
+
+        correoElectronico = self.browser.find_element_by_id('correoElectronico')
+        correoElectronico.send_keys('jorger@gmail.com')
+
+        contrasenia = self.browser.find_element_by_id('contrasenia')
+        contrasenia.send_keys('calve123')
+
+        foto = self.browser.find_element_by_id('foto')
+        foto.send_keys('C:\Users\jd.runza\Desktop\lfxfr5W.jpg')
+
+        botonGuardar = self.browser.find_element_by_id('id_grabar')
+        botonGuardar.click()
+        self.browser.implicitly_wait(3)
+        p = self.browser.find_element(By.XPATH, "//p[text()='Jorge Runza']")
+
+        self.assertIn('Jorge Runza', p.text)
